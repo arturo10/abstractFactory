ab1fba847a451dc63653ac381e4aa17557adf87a
ArturoH10 <artaurohack@hotmail.com>
2016-11-07 00:56:09 -0600

    commit prueba

--- src/test/java/products/BluRay_CapaDobleTest.java
+++ src/test/java/products/BluRay_CapaDobleTest.java
@@ -18,7 +18,7 @@ public class BluRay_CapaDobleTest {
 
     @Test
     public void getCapacidadTest(){
-        assertEquals("4.9GB21s",bluRay.getCapacidad());
+        assertEquals("4.9GB",bluRay.getCapacidad());
     }
 
     @Test
